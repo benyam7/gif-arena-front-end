abf1ec403ba821b5cd20e591ea9322d236e2fd68
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-08-30 16:05:25 +0300

    feat: connect wallet button with phantom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // Constants
 const TWITTER_HANDLE = "benyam_7";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const checkIfLoggedIn = async (solana) => {
+    try {
+      const res = await solana.connect({ onlyIfTrusted: true });
+      console.log("response", res);
+      console.log("Connected with publick key", res.publicKey.toBase58());
+      setWalletAddress(res.publicKey.toString());
+    } catch (e) {
+      if (e.code === 4001) {
+      }
+    }
+  };
+  const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
       if (solana) {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!");
+          checkIfLoggedIn(solana);
         }
       } else {
         alert("No, solana wallet found. Go get Phantom 👻");
@@ -22,24 +35,46 @@ const App = () => {
     }
   };
 
+  const connectWalletManually = async () => {
+    try {
+      const { solana } = window;
+      const res = await solana.connect();
+
+      setWalletAddress(res.publicKey.toString());
+    } catch (error) {
+      console.log("Error while trying to connect manullay", error);
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWalletManually}
+    >
+      Connect Wallet
+    </button>
+  );
+
   useEffect(() => {
-    const onLoad = () => {
+    const onLoad = async () => {
       checkIfWalletIsConnected();
     };
     // the reason why we're doing this is, currently Phantom team suggests waiting for
     // the window to fully finish loading before checking for the solana object.
+
     window.addEventListener("load", onLoad);
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -48,7 +83,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
